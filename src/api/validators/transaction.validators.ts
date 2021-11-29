5fc1e06bc96f52b34284b403260ee94ef735eb29
Stanisalv Pyatnicyn <pyatnicyn.stanislav@gmail.com>
2021-11-29 11:01:33 +0300

    Fixed createTransactionValidator

--- src/api/validators/transaction.validators.ts
+++ src/api/validators/transaction.validators.ts
@@ -4,7 +4,7 @@ import { handleValidationError } from '../errors/validation.error';
 export const getTransactionByIdValidator = [check('id').isUUID(), handleValidationError];
 
 export const createTransactionValidator = [
-  check('to_id').notEmpty().isUUID(),
+  check('id_to').notEmpty().isUUID(),
   check('amount').notEmpty().isNumeric(),
   handleValidationError,
 ];
