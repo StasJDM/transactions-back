78480ab4cc8b9ad9b03128a8829b4e75c512aae1
Stanisalv Pyatnicyn <pyatnicyn.stanislav@gmail.com>
2022-01-07 15:05:56 +0300

    Added CORS

--- index.ts
+++ index.ts
@@ -1,10 +1,20 @@
 import express, { Application, Request, Response } from 'express';
 import router from './src/api/routes';
 import { sequelize } from './src/db/models';
+import cors from 'cors';
 
 const app: Application = express();
 const port = 3000;
 
+const allowCrossDomain = (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+};
+
+app.use(cors());
+app.use(allowCrossDomain);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
